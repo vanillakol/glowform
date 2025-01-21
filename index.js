8ef724d59e6876fc5790519b113ca422f91a4150
vanillakol <160184591+vanillakol@users.noreply.github.com>
2025-01-20 22:23:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -413,17 +413,7 @@ app.get('/movies/:imdb', async (req, res) => {
                     </div>
                 </div>
 
-                 <script>
-                    ${generateEpisodeButtonsScript}
-                    updateEpisodes(1);
-
-                    function performSearch() {
-                        const query = document.getElementById('searchInput').value.trim();
-                        if (query) {
-                            window.location.href = '/search/' + encodeURIComponent(query);
-                        }
-                    }
-                </script>
+             
             </body>
             </html>
         `);
