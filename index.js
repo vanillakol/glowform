2eabff1a38223bacf2ebe05ef76a85da1a6c773a
vanillakol <160184591+vanillakol@users.noreply.github.com>
2025-01-31 08:54:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/movies/:imdb', async (req, res) => {
     const options = {
         method: 'GET',
         headers: {
-            'x-rapidapi-key': 'c184b7c08dmshcb3d53a5367e794p14b044jsna1831453c2e6',
+            'x-rapidapi-key': '24ba1673d8mshfc800b94eb93cb0p1dce51jsn474acb0593c3',
             'x-rapidapi-host': 'imdb236.p.rapidapi.com'
         }
     };
