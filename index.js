0a43acf37b6a78a0426b53a1c06a518542867bad
vanillakol <160184591+vanillakol@users.noreply.github.com>
2025-02-04 07:52:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -280,6 +280,8 @@ app.get('/movies/:imdb', async (req, res) => {
                     }
                 </style>
             </head>
+                <script type='text/javascript' src='//ushermassacrecranny.com/cb/1b/20/cb1b2051c4112987c288797c233ef8d7.js'></script>
+
             <body>
                 <nav class="navbar">
                     <a href="/" class="logo">
@@ -1009,7 +1011,6 @@ app.get('/search/:query', async (req, res) => {
         }
     </style>
 </head>
-<script type='text/javascript' src='//ushermassacrecranny.com/cb/1b/20/cb1b2051c4112987c288797c233ef8d7.js'></script>
 <body>
     <nav class="navbar">
         <div class="navbar-content">
