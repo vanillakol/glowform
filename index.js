e3e4f0859cac1a99750002952205a81d206f13a7
vanillakol <160184591+vanillakol@users.noreply.github.com>
2025-02-07 08:49:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -361,7 +361,7 @@ app.get('/shows/:imdb', async (req, res) => {
     const options = {
         method: 'GET',
         headers: {
-            'x-rapidapi-key': '24ba1673d8mshfc800b94eb93cb0p1dce51jsn474acb0593c3',
+            'x-rapidapi-key': '9fb9e2fd36mshe7b0ad331b6a8dfp1bec52jsn2ba9b0676d2d',
             'x-rapidapi-host': 'imdb236.p.rapidapi.com'
         }
     };
@@ -728,7 +728,7 @@ app.get('/search/:query', async (req, res) => {
     const options = {
         method: 'GET',
         headers: {
-            'x-rapidapi-key': '24ba1673d8mshfc800b94eb93cb0p1dce51jsn474acb0593c3',
+            'x-rapidapi-key': '9fb9e2fd36mshe7b0ad331b6a8dfp1bec52jsn2ba9b0676d2d',
             'x-rapidapi-host': 'imdb236.p.rapidapi.com',
         },
     };
@@ -752,7 +752,7 @@ app.get('/search/:query', async (req, res) => {
                 const optionsmov = {
                     method: 'GET',
                     headers: {
-                        'x-rapidapi-key': '24ba1673d8mshfc800b94eb93cb0p1dce51jsn474acb0593c3',
+                        'x-rapidapi-key': '9fb9e2fd36mshe7b0ad331b6a8dfp1bec52jsn2ba9b0676d2d',
                         'x-rapidapi-host': 'imdb236.p.rapidapi.com',
                     },
                 };
